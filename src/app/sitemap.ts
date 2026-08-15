5acf8120bd8114fb9322d28c521f5f23adceb159
kam-rep <kameshtech1@gmail.com>
2026-08-15 11:40:24 +0530

    build error

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -1,1 +1,10 @@
-    
+import type { MetadataRoute } from "next";
+
+export default function sitemap(): MetadataRoute.Sitemap {
+  return [
+    {
+      url: "https://ssm_builders_and_enterprises.com",
+      lastModified: new Date(),
+    },
+  ];
+}
