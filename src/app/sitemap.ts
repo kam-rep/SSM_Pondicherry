027daf1f7d7f82b6d8d84a4975061e9f340ff520
kam-rep <kameshtech1@gmail.com>
2026-08-15 11:50:57 +0530

    Update sitemap URL

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -3,7 +3,7 @@ import type { MetadataRoute } from "next";
 export default function sitemap(): MetadataRoute.Sitemap {
   return [
     {
-      url: "https://ssm_builders_and_enterprises.com",
+      url: "https://ssmbuildersandenterprises-dk6lst5jn-kamesh-v-portfolio.vercel.app",
       lastModified: new Date(),
     },
   ];
